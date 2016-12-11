7cc72be5be15f60832f1d1b669ef1c40ec0c811b
Will Brannon <will.brannon@gmail.com>
2016-12-11 10:46:52 +0000

    Better tests for create_statement

--- tests/testthat/test-create_statement.R
+++ tests/testthat/test-create_statement.R
@@ -1,82 +1,63 @@
 context("Create statement generation")
 
-# Always available because it's in package:stats
-test_that("glm objects are handled correctly", {
-  mod1 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-              data=datasets::iris)
-  gen <- create_statement(mod1, src_table="src", dest_table="dest")
-  res <- structure(paste0("CREATE TABLE \"dest\" AS SELECT id, 1.0 * 2.17126629215507 + \"Sepal.Width\" ",
-                          "* 0.495888938388551 + \"Petal.Length\" * 0.829243912234806 + \"Petal.Width\" ",
-                          "* -0.315155173326474 + CASE WHEN (\"Species\" = 'versicolor') THEN (1.0) ELSE ",
-                          "(0.0) END * -0.723561957780729 + CASE WHEN (\"Species\" = 'virginica') THEN ",
-                          "(1.0) ELSE (0.0) END * -1.02349781449083 FROM \"src\""), class = c("sql", "character"))
-  expect_equal(gen, res)
+mod <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+           data=datasets::iris)
+
+src_tbl <- "foo bar"
+src_schema <- "baz"
+src_catalog <- "quux"
+src <- fqtn(src_tbl, schema=src_schema, catalog=src_catalog)
+
+dest_tbl <- "foo1 bar1"
+dest_schema <- "baz1"
+dest_catalog <- "quux1"
+dest <- fqtn(dest_tbl, schema=dest_schema, catalog=dest_catalog)
+
+pk <- c("customerid", "state")
+
+ss <- select_statement(mod, src_table=src_tbl, src_schema=src_schema,
+                       src_catalog=src_catalog, pk=pk)
+cs <- create_statement(mod, src_table=src_tbl, src_schema=src_schema,
+                       src_catalog=src_catalog, dest_table=dest_tbl,
+                       dest_schema=dest_schema, dest_catalog=dest_catalog,
+                       pk=pk)
+
+test_that("the select statement is included", {
+  expect_match(cs, ss, fixed=TRUE)
+})
+
+test_that("table names are included", {
+  expect_match(cs, src, fixed=TRUE)
+  expect_match(cs, dest, fixed=TRUE)
+})
+
+
+test_that("DROP TABLE is generated", {
+  cs <- create_statement(mod, src_table=src_tbl, src_schema=src_schema,
+                         src_catalog=src_catalog, dest_table=dest_tbl,
+                         dest_schema=dest_schema, dest_catalog=dest_catalog,
+                         pk=pk, drop=TRUE)
   
-  mod2 <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-              data=datasets::iris, family=binomial("logit"))
-  gen <- create_statement(mod2, src_table="src", dest_table="dest")
-  res <- structure(paste0("CREATE TABLE \"dest\" AS SELECT id, 1.0 / (1.0 + EXP(-1.0 * (1.0 * -40.9486616902392 ",
-                          "+ \"Sepal.Width\" * 9.45599426269695 + \"Petal.Length\" * 5.37350821985281 ",
-                          "+ \"Petal.Width\" * 0.643343276782682 + CASE WHEN (\"Species\" = 'versicolor') THEN ",
-                          "(1.0) ELSE (0.0) END * 0.0945379643229317 + CASE WHEN (\"Species\" = 'virginica') THEN ",
-                          "(1.0) ELSE (0.0) END * -7.59422356504794))) FROM \"src\""), class = c("sql", "character"))
-  expect_equal(gen, res)
+  expect_match(cs, "DROP TABLE IF EXISTS", fixed=TRUE)
 })
 
-# Always available because it's in package:stats
-test_that("lm objects are handled correctly", {
-  mod1 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-             data=datasets::iris)
-  gen <- create_statement(mod1, src_table="src", dest_table="dest")
-  res <- structure(paste0("CREATE TABLE \"dest\" AS SELECT id, 1.0 * 2.17126629215507 + \"Sepal.Width\" ",
-                          "* 0.495888938388551 + \"Petal.Length\" * 0.829243912234806 + \"Petal.Width\" ",
-                          "* -0.315155173326474 + CASE WHEN (\"Species\" = 'versicolor') THEN (1.0) ELSE ",
-                          "(0.0) END * -0.723561957780729 + CASE WHEN (\"Species\" = 'virginica') THEN ",
-                          "(1.0) ELSE (0.0) END * -1.02349781449083 FROM \"src\""), class = c("sql", "character"))
-  expect_equal(gen, res)
+test_that("TEMPORARY is generated", {
+  cs <- create_statement(mod, src_table=src_tbl, src_schema=src_schema,
+                         src_catalog=src_catalog, dest_table=dest_tbl,
+                         dest_schema=dest_schema, dest_catalog=dest_catalog,
+                         pk=pk, temporary=TRUE)
+  
+  expect_match(cs, "TEMPORARY", fixed=TRUE)
 })
 
-# Only run if installed
-if("arm" %in% installed.packages())
-{
-  test_that("Bayesglm objects are handled correctly", {
-    mod1 <- arm::bayesglm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-                          data=datasets::iris)
-    gen <- create_statement(mod1, src_table="src", dest_table="dest")
-    res <- structure(paste0("CREATE TABLE \"dest\" AS SELECT id, 1.0 * 2.16746489171907 + \"Sepal.Width\" *",
-                            " 0.498241952725385 + \"Petal.Length\" * 0.826542543418053 + \"Petal.Width\" *",
-                            " -0.31800128132871 + CASE WHEN (\"Species\" = 'versicolor') THEN (1.0) ELSE (0.0) ",
-                            "END * -0.710871735643408 + CASE WHEN (\"Species\" = 'virginica') THEN (1.0) ELSE ",
-                            "(0.0) END * -1.00574899620441 FROM \"src\""), class = c("sql", "character"))
-    expect_equal(gen, res)
-  })
-}
-
-# Only run if installed
-if("mboost" %in% installed.packages())
-{
-  test_that("glmboost objects are handled correctly", {
-    mod1 <- mboost::glmboost(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-                             data=datasets::iris)
-    gen <- create_statement(mod1, src_table="src", dest_table="dest")
-    res <- structure(paste0("CREATE TABLE \"dest\" AS SELECT id, 1.0 * 2.47773332391217 + \"Sepal.Width\" *",
-                            " 0.536390255877286 + \"Petal.Length\" * 0.460907829277574 + CASE WHEN (\"Species\" =",
-                            " 'virginica') THEN (1.0) ELSE (0.0) END * -0.0192462659183535 FROM \"src\""),
-                     class = c("sql", "character"))
-    expect_equal(gen, res)
-  })
-}
-
-# Only run if installed
-if("glmnet" %in% installed.packages())
-{
-  test_that("cv.glmnet objects are handled correctly", {
-    mod1 <- glmnet::cv.glmnet(as.matrix(datasets::iris[, c("Sepal.Width", "Petal.Length", "Petal.Width")]),
-                              datasets::iris$Sepal.Length, nfolds=nrow(datasets::iris), grouped=FALSE)
-    gen <- create_statement(mod1, src_table="src", dest_table="dest")
-    res <- structure(paste0("CREATE TABLE \"dest\" AS SELECT id, 1.0 * 2.37744529257366 + \"Sepal.Width\" ",
-                            "* 0.556167083965938 + \"Petal.Length\" * 0.499465780735234 + \"Petal.Width\" ",
-                            "* -0.0929620855657578 FROM \"src\""), class = c("sql","character"))
-    expect_equal(gen, res)
-  })
-}
+test_that("pk columns are included", {
+  cs <- create_statement(mod, src_table=src_tbl, src_schema=src_schema,
+                         src_catalog=src_catalog, dest_table=dest_tbl,
+                         dest_schema=dest_schema, dest_catalog=dest_catalog,
+                         pk=pk, temporary=TRUE)
+  
+  for(p in pk)
+  {
+    expect_match(cs, p, fixed=TRUE)
+  }
+})
