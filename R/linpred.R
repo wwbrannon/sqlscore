2d1f5102006f30272f2db62a73ca8c7547711c39
William Brannon <will.brannon@gmail.com>
2016-12-11 10:46:52 +0000

    Examples for two functions

--- R/linpred.R
+++ R/linpred.R
@@ -10,10 +10,17 @@
 #' @return An unevaluated R call object representing the linear predictor.
 #' 
 #' @examples 
+#' # A Gaussian GLM including factors
 #' mod <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
 #'            data=datasets::iris)
 #' linpred(mod)
 #' 
+#' # A binomial GLM - linear predictor is unaffected
+#' mod <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+#'            data=datasets::iris, family=binomial("logit"))
+#' linpred(mod)
+#' 
+#' #With formula operators
 #' x <- matrix(rnorm(100*20),100,20)
 #' colnames(x) <- sapply(1:20, function(x) paste0("X", as.character(x)))
 #' x <- as.data.frame(x)
--- R/score_expression.R
+++ R/score_expression.R
@@ -12,6 +12,27 @@
 #' 
 #' @rdname score_expression
 #' 
+#' @examples 
+#' # A Gaussian GLM including factors
+#' mod <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+#'            data=datasets::iris)
+#' score_expression(mod)
+#' 
+#' # A binomial GLM - linear predictor is unaffected
+#' mod <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+#'            data=datasets::iris, family=binomial("logit"))
+#' score_expression(mod)
+#' 
+#' #With a hand-specified response function
+#' score_expression(mod, response="probit")
+#' 
+#' #With formula operators
+#' x <- matrix(rnorm(100*20),100,20)
+#' colnames(x) <- sapply(1:20, function(x) paste0("X", as.character(x)))
+#' x <- as.data.frame(x)
+#' mod <- glm(X2 ~ X3 + X5 + X15*X8, data=x)
+#' score_expression(mod)
+#' 
 #' @export
 score_expression <-
 function(mod, response=NULL)
