fc60feedb8b4e3eac2d4ab7d7249697c9eaa6c70
Will Brannon <will.brannon@gmail.com>
2016-12-11 10:46:52 +0000

    Clean up fqtn tests

--- tests/testthat/test-fqtn.R
+++ tests/testthat/test-fqtn.R
@@ -1,26 +1,26 @@
 context("Table name escaping")
 
 test_that("Basic table names are escaped", {
-  expect_equal(sqlscore:::fqtn("tbl"), dplyr::sql('"tbl"'))
-  expect_equal(sqlscore:::fqtn("foo", schema="bar"), dplyr::sql('"bar"."foo"'))
-  expect_equal(sqlscore:::fqtn("foo", schema="bar", catalog="baz"), dplyr::sql('"baz"."bar"."foo"'))
+  expect_equal(fqtn("tbl"), dplyr::sql('"tbl"'))
+  expect_equal(fqtn("foo", schema="bar"), dplyr::sql('"bar"."foo"'))
+  expect_equal(fqtn("foo", schema="bar", catalog="baz"), dplyr::sql('"baz"."bar"."foo"'))
 })
 
 test_that("More complicated names are escaped", {
-  expect_equal(sqlscore:::fqtn("foo bar"), dplyr::sql('"foo bar"'))
-  expect_equal(sqlscore:::fqtn("foo bar", schema="baz"), dplyr::sql('"baz"."foo bar"'))
-  expect_equal(sqlscore:::fqtn("foo bar", schema="baz", catalog="quux quux"),
+  expect_equal(fqtn("foo bar"), dplyr::sql('"foo bar"'))
+  expect_equal(fqtn("foo bar", schema="baz"), dplyr::sql('"baz"."foo bar"'))
+  expect_equal(fqtn("foo bar", schema="baz", catalog="quux quux"),
                dplyr::sql('"quux quux"."baz"."foo bar"'))
-  expect_equal(sqlscore:::fqtn("foo", schema="baz", catalog="quux quux"),
+  expect_equal(fqtn("foo", schema="baz", catalog="quux quux"),
                dplyr::sql('"quux quux"."baz"."foo"'))
 })
 
 test_that("Invalid calls throw errors", {
-  expect_error(sqlscore:::fqtn(NULL))
-  expect_error(sqlscore:::fqtn(0))
-  expect_error(sqlscore:::fqtn(""))
-  expect_error(sqlscore:::fqtn(catalog="bar")) #need table
-  expect_error(sqlscore:::fqtn("foo", catalog="bar")) #need schema
-  expect_error(sqlscore:::fqtn(schema="foo", catalog="bar")) #need table
+  expect_error(fqtn(NULL))
+  expect_error(fqtn(0))
+  expect_error(fqtn(""))
+  expect_error(fqtn(catalog="bar")) #need table
+  expect_error(fqtn("foo", catalog="bar")) #need schema
+  expect_error(fqtn(schema="foo", catalog="bar")) #need table
 })
 
