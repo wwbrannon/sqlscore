d7c71e1a5fe2ef61a7cd18f78a0ea9a53e2173bc
William Brannon <will.brannon@gmail.com>
2016-12-11 10:46:52 +0000

    Test updates

--- tests/testthat/test-extract_coef.R
+++ tests/testthat/test-extract_coef.R
@@ -1,24 +1,20 @@
 context("Coefficient extraction")
 
-# Set up objects shared across all tests
-mod1 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-            data=datasets::iris)
-
-mod2 <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width + Species,
-            data=datasets::iris, family=binomial("logit"))
-
-x <- matrix(rnorm(100*20),100,20)
-colnames(x) <- sapply(1:20, function(x) paste0("X", as.character(x)))
-x <- as.data.frame(x)
-mod3 <- glm(X2 ~ X3 + X5 + X15*X8, data=x)
-
 # Always available because it's in package:stats
 test_that("glm coefficients are extracted correctly", {
+  mod1 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+              data=datasets::iris)
+  
+  mod2 <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+              data=datasets::iris, family=binomial("logit"))
   
 })
 
 # Always available because it's in package:stats
 test_that("lm coefficients are extracted correctly", {
+  mod1 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+             data=datasets::iris)
+  
   
 })
 
@@ -28,7 +24,6 @@ if("arm" %in% installed.packages())
   test_that("bayesglm coefficients are extracted correctly", {
     
   })
-  
 }
 
 # Only run if installed
