0925e1d2f66f7b75847f9bdd1fe09386bdeac0f2
Will Brannon <will.brannon@gmail.com>
2016-12-11 10:46:52 +0000

    More tests

--- tests/testthat/test-extract_coef.R
+++ tests/testthat/test-extract_coef.R
@@ -4,18 +4,27 @@ context("Coefficient extraction")
 test_that("glm coefficients are extracted correctly", {
   mod1 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
               data=datasets::iris)
+  res <- c(`(Intercept)`=2.17126629215507, Sepal.Width=0.495888938388551,
+           Petal.Length=0.829243912234806, Petal.Width=-0.315155173326474,
+           Speciesversicolor=-0.723561957780729, Speciesvirginica=-1.02349781449083)
+  expect_equal(extract_coef(mod1), res) #to within numerical tolerance
   
   mod2 <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width + Species,
               data=datasets::iris, family=binomial("logit"))
-  
+  res <- c(`(Intercept)`=-40.9486616902392, Sepal.Width=9.45599426269695,
+           Petal.Length=5.37350821985281, Petal.Width=0.643343276782682,
+           Speciesversicolor=0.0945379643229317, Speciesvirginica=-7.59422356504794)
+  expect_equal(extract_coef(mod2), res) #to within numerical tolerance
 })
 
 # Always available because it's in package:stats
 test_that("lm coefficients are extracted correctly", {
   mod1 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
              data=datasets::iris)
-  
-  
+  res <- c(`(Intercept)`=2.17126629215507, Sepal.Width=0.495888938388551,
+           Petal.Length=0.829243912234806, Petal.Width=-0.315155173326474,
+           Speciesversicolor=-0.723561957780729, Speciesvirginica=-1.02349781449083)
+  expect_equal(extract_coef(mod1), res) #to within numerical tolerance
 })
 
 # Only run if installed
