f5d262ff3fa4e51b7d1823c2878a25bb7033ec89
William Brannon <will.brannon@gmail.com>
2017-05-08 20:22:02 -0400

    updates to tests

--- tests/testthat/test-extract_factors.R
+++ tests/testthat/test-extract_factors.R
@@ -5,15 +5,15 @@ test_that("glm coefficients are extracted correctly", {
   mod1 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
               data=datasets::iris)
   expect_equal(extract_factors(mod1), c("Species"))
-  
+
   mod2 <- glm(Sepal.Length > 5.0 ~ Sepal.Width + Petal.Length + Petal.Width,
               data=datasets::iris, family=binomial("logit"))
   expect_equal(extract_factors(mod2), character(0))
-  
+
   mod3 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
               data=datasets::iris, contrasts=list(Species="contr.poly"))
   expect_error(extract_factors(mod3))
-  
+
 })
 
 # Always available because it's in package:stats
@@ -21,7 +21,7 @@ test_that("lm coefficients are extracted correctly", {
   mod1 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
              data=datasets::iris)
   expect_equal(extract_factors(mod1), c("Species"))
-  
+
   mod2 <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
              data=datasets::iris)
   expect_equal(extract_factors(mod2), character(0))
@@ -34,7 +34,7 @@ if("arm" %in% installed.packages())
     mod1 <- arm::bayesglm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
                           data=datasets::iris)
     expect_equal(extract_factors(mod1), c("Species"))
-    
+
     mod2 <- arm::bayesglm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
                           data=datasets::iris)
     expect_equal(extract_factors(mod2), character(0))
@@ -48,7 +48,7 @@ if("mboost" %in% installed.packages())
     mod1 <- mboost::glmboost(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
                              data=datasets::iris)
     expect_equal(extract_factors(mod1), c("Species"))
-    
+
     mod2 <- mboost::glmboost(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width,
                              data=datasets::iris)
     expect_equal(extract_factors(mod2), character(0))
--- tests/testthat/test-extract_offsets.R
+++ tests/testthat/test-extract_offsets.R
@@ -5,12 +5,12 @@ test_that("glm offsets are extracted correctly", {
   mod1 <- glm(Sepal.Length ~ Sepal.Width + Petal.Length + offset(Petal.Width) + Species,
               data=datasets::iris)
   res <- c(Petal.Width=1)
-  expect_equal(extract_offsets(mod1), res)
-  
+  expect_equal(sort(extract_offsets(mod1)), sort(res))
+
   mod2 <- glm(Sepal.Length > 5.0 ~ Sepal.Width + offset(Petal.Length) + offset(Petal.Width) + Species,
               data=datasets::iris, family=binomial("logit"))
   res <- c(Petal.Length=1, Petal.Width=1)
-  expect_equal(extract_offsets(mod2), res)
+  expect_equal(sort(extract_offsets(mod2)), sort(res))
 })
 
 # Always available because it's in package:stats
@@ -18,7 +18,7 @@ test_that("lm offsets are extracted correctly", {
   mod1 <- lm(Sepal.Length ~ Sepal.Width + offset(Petal.Length) + offset(Petal.Width) + Species,
              data=datasets::iris)
   res <- c(Petal.Length=1, Petal.Width=1)
-  expect_equal(extract_offsets(mod1), res)
+  expect_equal(sort(extract_offsets(mod1)), sort(res))
 })
 
 # Only run if installed
@@ -28,12 +28,12 @@ if("arm" %in% installed.packages())
     mod1 <- arm::bayesglm(Sepal.Length ~ Sepal.Width + Petal.Length + offset(Petal.Width) + Species,
                           data=datasets::iris)
     res <- c(Petal.Width=1)
-    expect_equal(extract_offsets(mod1), res)
-    
+    expect_equal(sort(extract_offsets(mod1)), sort(res))
+
     mod2 <- arm::bayesglm(Sepal.Length > 5.0 ~ Sepal.Width + offset(Petal.Length) + offset(Petal.Width) + Species,
                 data=datasets::iris, family=binomial("logit"))
     res <- c(Petal.Length=1, Petal.Width=1)
-    expect_equal(extract_offsets(mod2), res)
+    expect_equal(sort(extract_offsets(mod2)), sort(res))
   })
 }
 
